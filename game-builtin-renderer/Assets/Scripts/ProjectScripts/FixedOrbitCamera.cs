08ed0e136798cff0bf0d316d61497825f7ce6fac
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-01-21 15:20:08 -0500

    overhaul camera system

--- game-builtin-renderer/Assets/Scripts/ProjectScripts/FixedOrbitCamera.cs
+++ game-builtin-renderer/Assets/Scripts/ProjectScripts/FixedOrbitCamera.cs
@@ -13,6 +13,9 @@ public class FixedOrbitCamera : MonoBehaviour
     [SerializeField]
     float _safeAngle = 35f; // degrees
 
+    [SerializeField]
+    float _minDistance = 2f; // meters away from focus before orbit trigger
+
     SphereCoords _sphereCoords;
 
     // Use this for initialization
@@ -29,23 +32,26 @@ public class FixedOrbitCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 toTarget = _trackedTarget.position - _focus.position;
-        Vector3 toCamera = transform.position - _focus.position;
-
-        float angleDifference = Vector3.Angle(toTarget, toCamera);
-        Vector3 linearDifference = toCamera - toTarget;
-
-        float sign = Mathf.Sign(Vector3.Dot(transform.right, linearDifference));
-
-        if (angleDifference > _safeAngle)
+        if ((_trackedTarget.position - _focus.position).magnitude > _minDistance)
         {
-            _sphereCoords.yaw += sign * angleDifference * Time.deltaTime; 
+            Vector3 toTarget = _trackedTarget.position - _focus.position;
+            Vector3 toCamera = transform.position - _focus.position;
+
+            float angleDifference = Vector3.Angle(toTarget, toCamera);
+            Vector3 linearDifference = toCamera - toTarget;
+
+            float sign = -1f * Mathf.Sign(Vector3.Dot(transform.right, linearDifference));
+
+            if (angleDifference > _safeAngle)
+            {
+                _sphereCoords.yaw += sign * angleDifference * Time.deltaTime;
+            }
+
+            transform.position = _focus.transform.position + _sphereCoords.GetRectFromSphere();
+
+            transform.LookAt(_focus, Vector3.up);
+            // transform.rotation *= Quaternion.FromToRotation(transform.forward, _focus.position - transform.position);
         }
-
-        transform.position = _focus.transform.position + _sphereCoords.GetRectFromSphere();
-
-        transform.LookAt(_focus, Vector3.up);
-        // transform.rotation *= Quaternion.FromToRotation(transform.forward, _focus.position - transform.position); 
     }
 
 
---
+++ game-builtin-renderer/Assets/Scripts/ProjectScripts/TransformCamManager.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GGJ2022
+{
+    // instead of manipulating a camera directly,
+    // wrap it in a transform and move that transform.
+    public class TransformCamManager : MonoBehaviour
+    {
+        [Tooltip("transform to manipulate")]
+        [SerializeField]
+        private GameObject _targetTransform;
+
+        [SerializeField]
+        private float _defaultTransitionTime = 0.8f;
+
+
+        public Transform startCam; // fallback if previousCam is undefined
+
+        private bool isTransitioning = false;
+        private Transform activeCam;
+        private Transform previousCam;
+
+        private Transform transitionStartTransform;
+
+        public static TransformCamManager instance = null;              //Static instance of CameraManager which allows it to be accessed by any other script.
+
+        public delegate void TransitionStarted(Transform newCam);
+        public delegate void TransitionEnded(Transform finalCam);
+
+        public TransitionStarted OnTransitionStarted;
+        public TransitionEnded OnTransitionEnded;
+
+        #region getters/setters
+
+        public bool GetIsTransitioning()
+        {
+            return isTransitioning;
+        }
+
+        public Transform GetActiveCam()
+        {
+            return activeCam;
+        }
+
+        public Transform GetPreviousCam()
+        {
+            return previousCam;
+        }
+        #endregion
+
+        #region monobehaviour 
+        void Awake()
+        {
+            #region singleton
+            //Check if instance already exists
+            if (instance == null)
+
+                //if not, set instance to this
+                instance = this;
+
+            //If instance already exists and it's not this:
+            else if (instance != this)
+
+                //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a CameraManager.
+                Destroy(gameObject);
+
+            //Sets this to not be destroyed when reloading scene
+            DontDestroyOnLoad(gameObject);
+            #endregion
+
+            // user can define their own camera if they want, otherwise we use this default
+            //if (transitionCam == null)
+            //{
+            //    transitionCam = gameObject.AddComponent<Transform>();
+            //}
+
+            if (startCam == null)
+            {
+                print("define a default camera in the inspector");
+            }
+
+            activeCam = startCam;
+
+            SwitchToCam(startCam);
+
+        }
+
+        private void Update()
+        {
+            // track the target to the active transform
+            // (it's possible that the active transform isn't static)
+            if (!isTransitioning)
+            {
+                _targetTransform.transform.SetPositionAndRotation(activeCam.transform.position,
+                                                                  activeCam.transform.rotation);
+
+            }
+        }
+
+        #endregion
+
+        #region methods
+        public void InstantSwitchToCam(Transform cam)
+        {
+            // instantaneous cam switch, no time delayed lerp
+
+            // check camera is real
+            if (cam != null)
+            {
+                previousCam = activeCam;
+                activeCam = cam;
+
+                _targetTransform.transform.SetPositionAndRotation(activeCam.transform.position,
+                                                                  activeCam.transform.rotation); 
+
+                // make sure manager state is 'not transitioning' 
+                isTransitioning = false;
+
+                OnTransitionEnded?.Invoke(activeCam);
+            }
+            else
+            {
+                print("transform to switch to doesn't exist");
+            }
+        }
+
+        public void SwitchToCam(Transform cam)
+        {
+            TimedSwitchToCam(cam, _defaultTransitionTime);
+        }
+
+        public void TimedSwitchToCam(Transform cam, float duration)
+        {
+            // time-delayed cam switch using coroutine
+
+            // check camera is real
+            if (cam != null)
+            {
+                // a transition might already be happening--need to handle that 
+                if (isTransitioning)
+                {
+                    // we're interrupting an in-progress transition 
+                    // so stop coroutine if running
+                    StopCoroutine("SwitchingCamRoutine");
+
+                    // set start transform to transitionCamera's current position 
+                    transitionStartTransform = _targetTransform.transform;
+                    OnTransitionEnded?.Invoke(_targetTransform.transform);
+                    // don't modify previousCam--leave it
+                }
+                else
+                {
+                    previousCam = activeCam;
+
+                    if (previousCam == null)
+                    {
+                        previousCam = startCam;
+                    }
+
+                    // transitionStartTransform = _targetTransform.transform;
+                }
+
+                isTransitioning = true;
+
+                previousCam = activeCam;
+                activeCam = cam;
+
+                OnTransitionStarted?.Invoke(_targetTransform.transform);
+
+                StartCoroutine(SwitchingCamRoutine(duration));
+            }
+            else
+            {
+                print("camera to switch to doesn't exist");
+            }
+        }
+
+        private IEnumerator SwitchingCamRoutine(float duration)
+        {
+            Debug.Log($"{duration} second transition starting at {Time.time}");
+
+            float timeStart = Time.time;
+            float timeEnd = timeStart + duration;
+
+            Vector3 startPos = _targetTransform.transform.position;
+            Quaternion startRot = _targetTransform.transform.rotation;
+
+            while (Time.time < timeEnd)
+            {
+                float t = 1f - (timeEnd - Time.time) / duration;
+                Debug.Log(t);
+                _targetTransform.transform.position = Vector3.Lerp(startPos, activeCam.transform.position, t);
+                _targetTransform.transform.rotation = Quaternion.Lerp(startRot, activeCam.transform.rotation, t);
+                yield return null;
+            }
+
+            isTransitioning = false;
+
+            OnTransitionEnded?.Invoke(activeCam);
+        }
+        #endregion
+
+    }
+}
