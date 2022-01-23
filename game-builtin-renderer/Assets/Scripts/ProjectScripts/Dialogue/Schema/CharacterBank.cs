a79cf7f775a7ad584fea2d4bea644feee663e050
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-01-23 17:10:12 -0500

    implemented dialogue schema

---
+++ game-builtin-renderer/Assets/Scripts/ProjectScripts/Dialogue/Schema/CharacterBank.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace GGJ2022.Dialogue.Schema
+{
+    [Serializable]
+    public class CharacterBank : ScriptableObject
+    {
+        List<Character> _characters;
+
+        public void Initialize(List<Character> characters)
+        {
+            _characters = characters; 
+        }
+    }
+}
+    
+
