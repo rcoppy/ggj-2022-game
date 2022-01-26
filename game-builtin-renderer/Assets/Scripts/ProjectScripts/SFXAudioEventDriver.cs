591a3057195d9b444c154d056205d16233701afe
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-01-26 16:49:43 -0500

    preliminary sfx implementation

---
+++ game-builtin-renderer/Assets/Scripts/ProjectScripts/SFXAudioEventDriver.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GGJ2022
+{
+    public class SFXAudioEventDriver : MonoBehaviour
+    {
+        // singleton pattern
+        public static SFXAudioEventDriver Instance { get; set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                Instance = this;
+            }
+
+            _sfxMapFile.RefreshMap();
+            _sfxDict = _sfxMapFile.Map;
+        }
+
+        [SerializeField]
+        GameObject _listener;
+
+        [SerializeField]
+        SFXFMODMap _sfxMapFile;
+
+        [SerializeField]
+        Dictionary<string, FMODUnity.EventReference> _sfxDict; 
+
+        [SerializeField]
+        RelativeCharacterController _playerController;
+
+        [SerializeField]
+        CharacterAnimationManager _playerAnimationManager;
+
+
+        public void FireSFXEvent(string action)
+        {
+            if (_sfxDict == null)
+            {
+                _sfxMapFile.RefreshMap();
+                _sfxDict = _sfxMapFile.Map; 
+            }
+
+            FMODUnity.EventReference path = _sfxDict[action];
+
+            FMODUnity.RuntimeManager.PlayOneShotAttached(path.Path, _listener);
+        }
+
+        private void OnEnable()
+        {
+            _playerController.OnJumpStarted.AddListener(() => FireSFXEvent("Jump"));
+            _playerController.OnJumpEnded.AddListener(() => FireSFXEvent("EndJump"));
+
+            _playerAnimationManager.OnActionStarted += FireSFXEvent;
+        }
+
+        private void OnDisable()
+        {
+            _playerController.OnJumpStarted.RemoveAllListeners();
+            _playerController.OnJumpEnded.RemoveAllListeners();
+
+            _playerAnimationManager.OnActionStarted -= FireSFXEvent;
+            
+
+
+        }
+    }
+}
---
+++ game-builtin-renderer/Assets/Scripts/ProjectScripts/SFXFMODMap.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace GGJ2022
+{
+    [CreateAssetMenu(fileName = "SFXMap", menuName = "GGJ2022/SFX event map", order = 1)]
+    public class SFXFMODMap : ScriptableObject
+    {
+        [Serializable]
+        public struct KeyValue
+        {
+            [SerializeField]
+            public string Action;
+
+            [SerializeField]
+            public FMODUnity.EventReference FMODEvent;
+        }
+
+        [SerializeField]
+        List<KeyValue> _keyValues;
+
+        Dictionary<string, FMODUnity.EventReference> _sfxMap;
+
+        public Dictionary<string, FMODUnity.EventReference> Map {
+            get { return _sfxMap; }
+        }
+
+        public void RefreshMap()
+        {
+            _sfxMap = new Dictionary<string, FMODUnity.EventReference>();
+
+            foreach (var kvp in _keyValues) {
+                _sfxMap[kvp.Action] = kvp.FMODEvent;
+            }
+
+        }
+    }
+}
