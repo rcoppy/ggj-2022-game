c29fad84f03db3adc4d6c2229fd3f4a2c7a670ff
Tucker Burke <tuckerwolfeburke@gmail.com>
2022-01-27 04:14:41 -0500

    Implemented the start button and credits screen functionality

---
+++ game-builtin-renderer/Assets/Scripts/MainMenu/NextScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public void Skip()
+    {
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
