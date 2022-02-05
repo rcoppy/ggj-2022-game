ab272e56e8a596ca0bc415318a82030fc94d7ecd
Alexander Rupp-Coppi <amr2311@columbia.edu>
2022-02-05 14:00:26 -0500

    asynchronous first level loading

--- game-builtin-renderer/Assets/Scripts/MainMenu/NextScene.cs
+++ game-builtin-renderer/Assets/Scripts/MainMenu/NextScene.cs
@@ -6,8 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
+    float _loadingStartTime;
+    public float MinLoadingTime = 1.5f; 
+
     public void Skip()
     {
-        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        StartCoroutine(LoadGameAsync());
     }
+
+    IEnumerator LoadGameAsync()
+    {
+        _loadingStartTime = Time.time;
+
+        // Start an asynchronous operation to load the scene
+        AsyncOperation async = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+
+        // Don't lead the scene start until all Studio Banks have finished loading
+        async.allowSceneActivation = false;
+
+
+        while (Time.time < _loadingStartTime + MinLoadingTime)
+        {
+            yield return null;
+        }
+
+        // Allow the scene to be activated. This means that any OnActivated() or Start()
+        // methods will be guaranteed that all FMOD Studio loading will be completed and
+        // there will be no delay in starting events
+        async.allowSceneActivation = true;
+
+        // Keep yielding the co-routine until scene loading and activation is done.
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+    }
+
 }
